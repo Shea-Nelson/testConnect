5b4b1a81227e43e2320c084ba56172085c69bd3a
Shea-Nelson <shea.nelson@icloud.com>
2021-07-31 09:49:34 -0400

    Create codeRefresh.ps1

---
+++ powershell/codeRefresh.ps1
@@ -0,0 +1,19 @@
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.IncludeSubdirectories = $true
+$watcher.Path = 'C:\Users\nelsons\Documents\GitHub\testConnect\.git'
+$watcher.Filter = 'ORIG_HEAD*.*'
+$watcher.EnableRaisingEvents = $true
+$destination = '\\dorsp19.fdor.dor.state.fl.us@SSL\DavWWWRoot\sites\gta\SiteAssets\devCode\pageTemplates\testConnect'
+$action =
+{
+$path = 'C:\Users\nelsons\Documents\GitHub\testConnect'
+$changetype = $event.SourceEventArgs.ChangeType
+Write-Host "$path was REFRESHED at $(get-date)"
+robocopy $path $destination /e /xo /xd .git /xf .gitattributes
+$wshell = New-Object -ComObject wscript.shell
+if($wshell.AppActivate('Chrome')) { # Switch to Chrome
+Sleep 1 # Wait for Chrome to "activate"
+$wshell.SendKeys("^R")  # Send CTRL + R (Refresh)
+}
+}
+Register-ObjectEvent $watcher 'Created' -Action $action
