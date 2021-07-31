f558c1b2c27eccbcaa0ec1f6510e59ff6a1ad31e
Shea-Nelson <shea.nelson@icloud.com>
2021-07-31 09:56:11 -0400

    Update codeRefresh.ps1

--- powershell/codeRefresh.ps1
+++ powershell/codeRefresh.ps1
@@ -6,14 +6,15 @@ $watcher.EnableRaisingEvents = $true
 $destination = '\\dorsp19.fdor.dor.state.fl.us@SSL\DavWWWRoot\sites\gta\SiteAssets\devCode\pageTemplates\testConnect'
 $action =
 {
-$path = 'C:\Users\nelsons\Documents\GitHub\testConnect'
-$changetype = $event.SourceEventArgs.ChangeType
-Write-Host "$path was REFRESHED at $(get-date)"
-robocopy $path $destination /e /xo /xd .git /xf .gitattributes
-$wshell = New-Object -ComObject wscript.shell
-if($wshell.AppActivate('Chrome')) { # Switch to Chrome
-Sleep 1 # Wait for Chrome to "activate"
-$wshell.SendKeys("^R")  # Send CTRL + R (Refresh)
-}
+    $path = 'C:\Users\nelsons\Documents\GitHub\testConnect'
+    # $changetype = $event.SourceEventArgs.ChangeType
+    Write-Host "$path was REFRESHED at $(get-date)"
+    robocopy $path $destination /e /xo /xd .git /xf .gitattributes
+    $wshell = New-Object -ComObject wscript.shell
+    if ($wshell.AppActivate('Chrome')) {
+        # Switch to Chrome
+        Start-Sleep 1 # Wait for Chrome to "activate"
+        $wshell.SendKeys("^R")  # Send CTRL + R (Refresh)
+    }
 }
 Register-ObjectEvent $watcher 'Created' -Action $action
